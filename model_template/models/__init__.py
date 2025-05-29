1686a4dc589009d6289d3daeeecdcddb204d6072
IBatina <36521002+IBatina@users.noreply.github.com>
2025-05-29 22:52:45 +0100

    fix model

--- model_template/models/__init__.py
+++ model_template/models/__init__.py
@@ -5,9 +5,5 @@ from .aalto_model import AaltoModel
 from .openAI_model import OpenAIModel
 from .testModel import TestModel
 from .o1mini_model import OpenAIImageModel as o1mini
-<<<<<<< Updated upstream
-from .o1_model import OpenAIImageModel as o1
-=======
 from .o1_model import OpenAIImageModel as o1
 from .simpleGPTAgent import CustomAgent as simpleAgent
->>>>>>> Stashed changes
