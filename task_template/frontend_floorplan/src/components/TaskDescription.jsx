4a2227f8461e5461e2a5cd5c526870dc0758ee11
bxbach732 <buixuanbach2000@gmail.com>
2025-08-11 08:30:20 +0300

    fix: modify the task description of floorplan

--- task_template/frontend_floorplan/src/components/TaskDescription.jsx
+++ task_template/frontend_floorplan/src/components/TaskDescription.jsx
@@ -5,13 +5,14 @@ const TaskDescription = () => {
       <h3>📜 Rules</h3>
       <ol>
         {/* <li>Provide the description of the room.</li> */}
-        <li>You are designing a bedroom with the user</li>
+        <li>You are designing a bedroom with the AI</li>
         {/* <li>The AI then gives you an initial floor plan.</li>
         <li>If you want to discuss with the AI about the floor plan, you can do that in the Conversation box.</li>
         <li>When you are sastified with the floor plan. Please then rate your experience with the AI.</li> */}
+        <li>The AI gives instructions on how to place the next piece</li>
         <li>You make the first move then the AI plays its move</li>
-        <li>If you want to discuss with the AI about the floor plan, you can do that in the Conversation box.</li>
-        <li>When you are sastified with the floor plan. Please then rate your experience with the AI.</li>
+        <li>If you want to discuss with the AI about the floor plan, you can do that in the Conversation box</li>
+        <li>When you are sastified with the floor plan. Please then rate your experience with the AI</li>
       </ol>
     </div>
   );
