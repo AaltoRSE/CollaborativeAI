b67f37dfbe208243187c7844d6c6bba3bf21e5e9
bxbach732 <buixuanbach2000@gmail.com>
2025-08-17 21:42:43 +0300

    chore: update floorplan task description for macos trackpad users

--- task_template/frontend_floorplan/src/components/TaskDescription.jsx
+++ task_template/frontend_floorplan/src/components/TaskDescription.jsx
@@ -12,6 +12,7 @@ const TaskDescription = () => {
         <li>The AI gives instructions on how to place the next piece</li>
         <li>You make the first move then the AI plays its move</li>
         <li>To rotate or delete a piece from the grid, right click on that piece to open a drop-down menu and then select the option</li>
+        <li>For macOS trackpad users, ensure the "Secondary click" option inside the Trackpad setting is not set to "Off". Use the set option there to open the drop-down menu for rotating and removing pieces.</li>
         <li>If you want to discuss with the AI about the floor plan, you can do that in the Conversation box</li>
         <li>When you are sastified with the floor plan. Please then rate your experience with the AI</li>
       </ol>
